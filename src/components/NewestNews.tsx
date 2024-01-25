9eb7b8b2a5f55e08043d991c44103d0fe9cfc361
Metodij-Gorgickov-FE11 <metodijgjorgichkov@yahoo.com>
2024-01-25 14:13:35 +0100

    finished the newestNews

--- src/components/NewestNews.tsx
+++ src/components/NewestNews.tsx
@@ -1,40 +1,80 @@
 import React from "react";
 
 const NewestNews = () => {
+  const NewsTypes = [
+    {
+      img: "/images/Homepage/homepage_card_2.jpg",
+      title: "еко-action",
+      date: "22.02.2023",
+      description:
+        "Lorem ipsum dolor sit amet consectetur. Curabitur cursus quis id.",
+    },
+    {
+      img: "/images/Homepage/homepage_card_2.jpg",
+      title: "еко-action",
+      date: "22.02.2023",
+      description:
+        "Lorem ipsum dolor sit amet consectetur. Curabitur cursus quis id.",
+    },
+    {
+      img: "/images/Homepage/homepage_card_2.jpg",
+      title: "еко-action",
+      date: "22.02.2023",
+      description:
+        "Lorem ipsum dolor sit amet consectetur. Curabitur cursus quis id.",
+    },
+    {
+      img: "/images/Homepage/homepage_card_2.jpg",
+      title: "еко-action",
+      date: "22.02.2023",
+      description:
+        "Lorem ipsum dolor sit amet consectetur. Curabitur cursus quis id.",
+    },
+    {
+      img: "/images/Homepage/homepage_card_2.jpg",
+      title: "еко-action",
+      date: "22.02.2023",
+      description:
+        "Lorem ipsum dolor sit amet consectetur. Curabitur cursus quis id.",
+    },
+  ];
   return (
-    <div className="newestNews container">
-      <div className="row">
-        <div className="col-12 mb-5">
-          <h2 className="font-weight-800">Најнови вести</h2>
-        </div>
-
-        <div className="col-12 position-relative">
-          <div className="d-flex align-items-center">
-            <img
-              src="/images/Homepage/homepage_card_2.jpg"
-              alt=""
-              className="border-radius"
-              style={{ width: "400px" }}
-            />
-            <div className="img-content p-3">
-              <div className="d-flex align-items-center pb-3">
-                <a href="#" className="text-white mr-3 px-1">
-                  еко-action
-                </a>
-                <img src="/images/Homepage/dateIcon.png" alt="date" />
-                <span className="text-white ml-1">25.08.2023</span>
-              </div>
-              <span className="text-white d-block mb-1">
-                Lorem ipsum dolor sit amet consectetur. Curabitur cursus quis
-                id.
-              </span>
-
-              <span className="d-block text-white">Види повеќе</span>
-            </div>
+    <>
+      <div className="newestNews container">
+        <div className="row">
+          <div className="col-12 mb-5">
+            <h2 className="font-weight-800">Најнови вести</h2>
           </div>
         </div>
+
+        <div className="scroll-section">
+          {NewsTypes.map((news) => {
+            return (
+              <div className="card-30 position-relative">
+                <img
+                  className="border-radius"
+                  src={news.img}
+                  alt={news.description}
+                />
+
+                <div className="img-content px-3 text-white">
+                  <a className="bg-orange text-dark mr-2" href="#">
+                    {news.title}
+                  </a>
+                  <span>25.02.2023</span>
+
+                  <p className="py-2 mb-0">{news.description}</p>
+
+                  <a className="text-white" href="#">
+                    Види повеќе
+                  </a>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
