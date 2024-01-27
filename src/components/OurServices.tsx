3ac3d523050cc62e8d235b0e763dbccdc481c87e
Metodij-Gorgickov-FE11 <metodijgjorgichkov@yahoo.com>
2024-01-27 15:31:19 +0100

    added some changes

--- src/components/OurServices.tsx
+++ src/components/OurServices.tsx
@@ -1,18 +1,20 @@
-import { title } from "process";
 import React from "react";
 
 const OurServices = () => {
   const OurServicesType = [
     {
       title: "01 Мултифункционален центар Крикни",
+      img: "/icons/blackArrow.png",
       desc: "Lorem ipsum dolor sit amet consectetur. Non at at risus dolor accumsan vitae dignissim. Est dapibus turpis metus ac rhoncus tellus volutpat. ",
     },
     {
       title: "02 Независни станбени единици",
+      img: "/icons/redArrow.png",
       desc: "Lorem ipsum dolor sit amet consectetur. Non at at risus dolor accumsan vitae dignissim. Est dapibus turpis metus ac rhoncus tellus volutpat. ",
     },
     {
       title: "03 советувалиште за Млади и Родители",
+      img: "/icons/redArrow.png",
       desc: "Lorem ipsum dolor sit amet consectetur. Non at at risus dolor accumsan vitae dignissim. Est dapibus turpis metus ac rhoncus tellus volutpat. ",
     },
   ];
@@ -30,10 +32,18 @@ const OurServices = () => {
           />
         </div>
         <div className="col-6">
-          {OurServicesType.map((service) => {
+          {OurServicesType.map((service, index) => {
             return (
-              <div className="right-column mb-3">
-                <h4>{service.title}</h4>
+              <div key={index} className="right-column mb-3">
+                <div className="d-flex align-items-center justify-content-between">
+                  <h4>{service.title}</h4>
+                  <img
+                    src={service.img}
+                    alt={service.title}
+                    style={{ width: "40px" }}
+                    className="cursor-pointer"
+                  />
+                </div>
                 <span
                   className="d-block pb-3"
                   style={{ borderBottom: "1px solid black" }}
