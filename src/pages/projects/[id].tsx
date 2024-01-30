679b1cfe42405ee514a86f05873d828f0bbecc2f
Metodij-Gorgickov-FE11 <metodijgjorgichkov@yahoo.com>
2024-01-30 17:24:22 +0100

    made some changes

--- src/pages/projects/[id].tsx
+++ src/pages/projects/[id].tsx
@@ -9,15 +9,42 @@ interface Props {
 const ProjectDetail = ({ projectData }: Props) => {
   const [currentIndex, setCurrentIndex] = useState<number>(0);
 
-  const goToNext = () => {};
-
-  const goToPrevious = () => {};
+  const goToPrevious = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide
+      ? projectData.project_images.length - 1
+      : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+  const goToNext = () => {
+    const isLastSlide = currentIndex === projectData.project_images.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
 
   return (
-    <div className="container">
+    <div className="project-detail container mt-4 py-5">
       <div className="row">
-        <div className="col-12">
-          <img src={projectData.project_images[currentIndex]} alt="" />
+        <div className="col-12 position-relative">
+          <img
+            src={projectData.project_images[currentIndex]}
+            alt={currentIndex.toString()}
+            className="project-detail-img"
+          />
+          {/* right arrow */}
+          <img
+            onClick={goToNext}
+            src="/icons/rightArrow.png"
+            alt="right arrow"
+            className="arrow right-arrow cursor-pointer"
+          />
+          {/* left arrow */}
+          <img
+            onClick={goToPrevious}
+            src="/icons/leftArrow.png"
+            alt="left arrow"
+            className="arrow left-arrow cursor-pointer"
+          />
         </div>
       </div>
     </div>
