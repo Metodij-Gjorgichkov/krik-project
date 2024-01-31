83e24f4a44a4c13657f1132344b6693551c41d0b
Metodij-Gorgickov-FE11 <metodijgjorgichkov@yahoo.com>
2024-01-31 14:56:06 +0100

    installed the react icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,18 +8,18 @@ const Footer = () => {
       style={{ borderTopLeftRadius: "10px", borderTopRightRadius: "10px" }}
     >
       <div className="row p-5">
-        <div className="col-4 text-center">
+        <div className="col-12 col-lg-4 text-center">
           <h3 className="text-white font-weight-800">
             Месечен <br /> билтен
           </h3>
         </div>
-        <div className="col-2 text-white">
+        <div className="col-12 col-lg-2 text-white text-center text-lg-left mb-4 mb-lg-0">
           <span>
             Lorem ipsum dolor sit <br /> amet consectetur. Non <br /> at at
             risus dolor <br /> accumsan vitae <br /> dignissim.
           </span>
         </div>
-        <div className="col-5">
+        <div className="col-12 col-lg-5">
           <a
             href="#"
             className="footer-btn py-1 px-1 d-block mb-2 border-radius text-white"
@@ -28,14 +28,14 @@ const Footer = () => {
           </a>
           <a
             href="#"
-            className="bg-orange py-1 px-1 d-block border-radius text-center"
+            className="bg-orange py-1 px-1 d-block border-radius text-center text-lg-left"
           >
             CTA
           </a>
         </div>
 
         <div className="footer-line"></div>
-        <div className="col-5 px-5 text-center">
+        <div className="col-12 col-lg-5 px-5 text-center mb-3 mb-lg-0">
           <h3 className="text-white font-weight-800">Контактирај не!</h3>
           <a
             href="#"
@@ -56,14 +56,14 @@ const Footer = () => {
             CTA
           </a>
         </div>
-        <div className="col-2 text-white">
+        <div className="col-12 col-lg-2 text-white text-center text-lg-left mb-3 mb-lg-0">
           <h4 className="mb-4">Приклучи се</h4>
           <span>
             Lorem ipsum dolor sit <br /> amet consectetur. Non <br /> at at
             risus dolor <br /> accumsan vitae <br /> dignissim.
           </span>
         </div>
-        <div className="col-5 text-white">
+        <div className="col-12 col-lg-5  mb-3 mb-lg-0 text-center text-white">
           <h4 className="mb-4">За Крик</h4>
           <span className="d-block mb-4">
             Lorem ipsum dolor sit amet consectetur. <br /> Non at at risus dolor
